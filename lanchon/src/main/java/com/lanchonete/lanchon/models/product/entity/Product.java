bbe618b0b88eee9e229c6aa53f7349452504b11e
Leandro-Oliveira-2 <leandro.oliveira@dcx.ufpb.br>
2025-09-21 12:27:25 -0300

    entidade produto criada, faltando apenas relacionamento

---
+++ lanchon/src/main/java/com/lanchonete/lanchon/models/product/entity/Product.java
@@ -0,0 +1,37 @@
+package com.lanchonete.lanchon.models.product.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private double price;
+
+    @NotNull
+    private Boolean active;
+
+}
