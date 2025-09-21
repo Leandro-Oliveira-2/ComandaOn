3000f7d612756093dfbf70c9ede5c422b897e73d
Leandro-Oliveira-2 <leandro.oliveira@dcx.ufpb.br>
2025-09-21 13:12:31 -0300

    Entidade item criada, faltando agora apenas o relacionamento

---
+++ lanchon/src/main/java/com/lanchonete/lanchon/models/item/entity/Item.java
@@ -0,0 +1,36 @@
+package com.lanchonete.lanchon.models.item.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private double price;
+
+    @NotNull
+    private int quantity;
+
+    @NotNull
+    private String notes;
+}
